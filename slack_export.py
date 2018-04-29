7bff15c7430e47fb6b3d2768226de7174e73ccc2
anji <anji6789@gmail.com>
2018-04-29 19:58:43 -0400

    multiple channel ids. reqs file.

--- slack_export.py
+++ slack_export.py
@@ -10,39 +10,45 @@ load_dotenv(find_dotenv())
 
 
 SLACK_TOKEN = os.getenv('SLACK_TOKEN')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+CHANNEL_IDS_STRING = os.getenv('CHANNEL_IDS')
+CHANNEL_IDS = CHANNEL_IDS_STRING.split(',')
 
-URL = 'https://slack.com/api/conversations.history'
+HISTORY_URL = 'https://slack.com/api/conversations.history'
+CHANNEL_INFO_URL = 'https://slack.com/api/conversations.info'
 
-payload = {
-    'token': SLACK_TOKEN,
-    'channel': CHANNEL_ID,
-    'limit': 200,
-}
+# channel_info_payload = {
+#     'token': SLACK_TOKEN,
+#     'channel': CHANNEL_ID
+# }
 
-filename = 'slack_dump_%s_%d.txt' % (CHANNEL_ID, arrow.utcnow().timestamp)
 
-cursor = True
-
-loops = 0
-
-with open(filename, 'w') as openfile:
-    while cursor:
-        response = requests.get(URL, payload)
-        if response.status_code != 200:
-            print response.status_code, response.text
-            raise Exception(response.status_code)
-        data = response.json()
-        messages = data.get('messages') or []
-        for message in messages:
-            line = ujson.dumps(message)
-            openfile.write(line + '\n')
-        cursor = None
-        if data.get('has_more'):
-            if data.get('response_metadata'):
-                cursor = data.get('response_metadata').get('next_cursor')
-                payload['cursor'] = cursor
-        loops += 1
-        if loops % 20 == 0:
-            print loops, message['ts']
-        time.sleep(.5)
+for channel_id in CHANNEL_IDS:
+    print 'processing channel %s' % channel_id
+    cursor = True
+    loops = 0
+    payload = {
+        'token': SLACK_TOKEN,
+        'channel': channel_id,
+        'limit': 200,
+    }
+    filename = 'conversation_%s_%d.txt' % (channel_id, arrow.utcnow().timestamp)
+    with open(filename, 'w') as openfile:
+        while cursor:
+            response = requests.get(HISTORY_URL, payload)
+            if response.status_code != 200:
+                print response.status_code, response.text
+                raise Exception(response.status_code)
+            data = response.json()
+            messages = data.get('messages') or []
+            for message in messages:
+                line = ujson.dumps(message)
+                openfile.write(line + '\n')
+            cursor = None
+            if data.get('has_more'):
+                if data.get('response_metadata'):
+                    cursor = data.get('response_metadata').get('next_cursor')
+                    payload['cursor'] = cursor
+            loops += 1
+            if loops % 20 == 0:
+                print loops, message['ts']
+            time.sleep(.5)
